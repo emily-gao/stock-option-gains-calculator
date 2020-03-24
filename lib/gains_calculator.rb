06e913d2f92efa60d7b36df174e03b1711cdcb62
Emily Gao <emily.s.gao@gmail.com>
2020-03-24 08:27:42 -0600

    always display two decimal points

--- lib/gains_calculator.rb
+++ lib/gains_calculator.rb
@@ -4,12 +4,19 @@ class GainsCalculator
 
     processed_records.each do |employee_id, data|
       unrealized_gain_sum = 0
+
       data[:grants].each do |grant|
+        # Do not include grants that have not vested yet
+        # Do not include grants whose grant prices is equal to or more than market price
         next if grant[:vest_date] > base_date || grant[:grant_price] >= market_price
 
-        unrealized_gain_sum += (grant[:units_left] * (market_price - grant[:grant_price])).round(2)
+        unrealized_gain_sum += grant[:units_left] * (market_price - grant[:grant_price])
       end
-      output << "#{employee_id},#{unrealized_gain_sum / 100},#{data[:realized_gain] / 100}\n"
+
+      # Divide sum by 100 since prices are records in cents
+      output << "#{employee_id},"\
+        "#{'%.2f' % (unrealized_gain_sum / 100.to_f)},"\
+        "#{'%.2f' % (data[:realized_gain] / 100.to_f)}\n"
     end
 
     output
