f7a8e34c2c58a9b2d40e8409cf6cc15603aec222
Emily Gao <emily.s.gao@gmail.com>
2020-03-24 08:28:04 -0600

    fix syntax error: wrong reference

--- lib/record_count_validator.rb
+++ lib/record_count_validator.rb
@@ -3,8 +3,8 @@ class RecordCountValidator
     return if provided == actual
 
     abort(
-      "ABORTED! Provided record count is #{provided_record_count} "\
-      "but actual record count is #{actual_record_count}."
+      "ABORTED! Provided record count is #{provided} "\
+      "but actual record count is #{actual}."
     )
   end
 end
