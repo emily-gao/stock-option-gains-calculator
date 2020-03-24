a7efcd88fd2318af55f5e3c4ad97058986c125c6
Emily Gao <emily.s.gao@gmail.com>
2020-03-24 15:09:54 -0600

    use price as 3rd criterion for sorting

--- spec/records_sorter_spec.rb
+++ spec/records_sorter_spec.rb
@@ -2,17 +2,20 @@ require './lib/records_sorter'
 
 describe RecordsSorter do
   describe '.call' do
-    let(:mapped_records) { [record_1, record_2, record_3, record_4] }
+    let(:mapped_records) { [record_1, record_2, record_3, record_4, record_5] }
     let(:earlier_date) { '20200301' }
     let(:later_date) { '20200401' }
     let(:vest) { 'VEST' }
     let(:perf) { 'PERF' }
     let(:sale) { 'SALE' }
+    let(:lower_price) { 500 }
+    let(:higher_price) { 1000 }
     let(:record_1) { { date: later_date, type: sale } }
     let(:record_2) { { date: later_date, type: perf } }
     let(:record_3) { { date: later_date, type: vest } }
-    let(:record_4) { { date: earlier_date, type: perf } }
-    let(:sorted_records) { [record_4, record_3, record_2, record_1] }
+    let(:record_4) { { date: earlier_date, type: perf, price: higher_price } }
+    let(:record_5) { { date: earlier_date, type: perf, price: lower_price } }
+    let(:sorted_records) { [record_5, record_4, record_3, record_2, record_1] }
     let(:call_sorter) { described_class.call(mapped_records) }
 
     it 'returns sorted records' do
